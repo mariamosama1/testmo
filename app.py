57455a77aa2b67576ddcfe6970dab7e8a9c3cf6b
mariamosama1 <m8037632@gmail.com>
2026-07-02 14:42:58 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import numpy as np
+import cv2
+from PIL import Image
+from tensorflow.keras.models import load_model
+import joblib
+import streamlit as st
+deep_model = load_model("brain_tumor_mlp.keras")
+encoder = joblib.load("label_encoder.pkl")
 st.set_page_config(page_title="Brain Tumor MRI Classifier")
 st.title("Brain Tumor MRI Classification")
 st.write("Upload brain MRI image to classify")
@@ -15,5 +23,27 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption= "Uploaded MRI Image", use_container_width=True)
 
-if st.button("Predict"):
-    st.success("Prediction will appear here after model is added.")
+if uploaded_file is not None and st.button("Predict"):
+
+    # Convert uploaded image to grayscale
+    img = image.convert("L")
+
+    # Resize to 64x64
+    img = img.resize((64, 64))
+
+    # Convert to NumPy array
+    img = np.array(img)
+
+    # Normalize (only if you normalized during training)
+    img = img / 255.0
+
+    # Flatten to 4096 features
+    img = img.reshape(1, 4096)
+
+    # Predict
+    prediction = deep_model.predict(img)
+
+    predicted_class = np.argmax(prediction, axis=1)[0]
+    predicted_label = encoder.inverse_transform([predicted_class])[0]
+
+    st.success(f"Predicted Tumor Type: {predicted_label}")
